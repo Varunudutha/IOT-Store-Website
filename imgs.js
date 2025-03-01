8327d3fc654068818b5bc1c2eb70809d353e6e03
varunudutha <137369464+Varunudutha@users.noreply.github.com>
2025-03-02 01:20:28 +0530

    Added Log in and Sign Up pages

---
+++ imgs.js
@@ -0,0 +1,21 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slider")[0].getElementsByTagName("img");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
